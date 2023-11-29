3643e04fcc79765b3083aa19d2b4325a1ba628e2
kadu91878 <kadu_91878@hotmail.com>
2023-11-29 16:38:03 -0300

    feat: initial commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import './assets/Style/style.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
+
+import App from './App.vue'
+import router from './router'
+
+
+// library.add(faUserSecret)
+
+const app = createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
