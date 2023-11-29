03bd3fcf025e6a50435a52367c32f507c34666da
kadu91878 <kadu_91878@hotmail.com>
2023-11-29 16:38:16 -0300

    feat: navBar test

---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="navigation-bar">
+    <div class="top-container">
+      <div class="top-box">
+        <div class="whatsapp-box">
+          <i class="fa-brands fa-whatsapp"></i>
+          <p>fale pelo whatsapp</p>
+        </div>
+        <div class="doubt-box">
+          <i class="fa-regular fa-circle-question"></i>
+          <p>tire suas duvidas</p>
+        </div>
+        <div class="top-left-box">
+          <div class="search-bar">
+            <input type="text" placeholder="Pesquisar" />
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </div>
+          <div class="cart">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <p>0</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom-container">
+      <div class="bottom-box">
+        <img src="../assets/images/Shehili.png" alt="logo" />
+        <ul class="nav-item">
+          <li><a href="#home">home</a></li>
+          <li><a href="#loja">loja</a></li>
+          <li><a href="#sobre">sobre</a></li>
+          <li><a href="#contato">contato</a></li>
+        </ul>
+        <div class="login">
+          <i class="fa-solid fa-circle-user"></i>
+          <p>login</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
