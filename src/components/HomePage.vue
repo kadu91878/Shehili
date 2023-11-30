d37dfcf43ba1c37977ad243d72f00e5cd09f859d
kadu91878 <kadu_91878@hotmail.com>
2023-11-30 12:48:12 -0300

    feat: add home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home-page section">
+    <div class="banner-container">
+      <div class="head-title">
+        <span>Produtos</span>
+      </div>
+      <div class="sub-title">
+        <p>Lorem ipsum dolor sit.</p>
+      </div>
+      <div class="banner">
+        <div class="banner-links">
+          <div class="circle-link">
+            <img src="../assets/images/pexels-photo-1148957.jpeg" alt="" />
+            <a href="#"> roupas</a>
+          </div>
+          <div class="circle-link">
+            <img src="../assets/images/pexels-photo-699122.jpeg" alt="" />
+            <a href="#">contato</a>
+          </div>
+          <div class="circle-link">
+            <img src="../assets/images/pexels-photo-3119215.jpeg" alt="" />
+            <a href="#">provador</a>
+          </div>
+          <div class="circle-link">
+            <img src="../assets/images/pexels-photo-4506243.jpeg" alt="" />
+            <a href="#">entregas</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import NavigationBar from '@/components/NavigationBar.vue';
+import HomePage from '@/components/HomePage.vue';
 </script>
 
 <template>
  <NavigationBar/>
+ <HomePage />
 </template>
