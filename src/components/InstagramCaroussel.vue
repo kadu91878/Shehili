fb25fd777e4742bb60e96dae16a445a46021c83a
kadu91878 <kadu_91878@hotmail.com>
2023-12-01 23:28:08 -0300

    feat: caroussel v0.2

--- src/components/InstagramCaroussel.vue
+++ src/components/InstagramCaroussel.vue
@@ -7,18 +7,22 @@
       <h2>Shehili</h2>
     </div>
     <div class="instagram-container">
-      <div class="instagram-caroussel">
-        <div class="insta-image" v-for="image in instagramImages">
-          <img :src="image" alt="" />
-          <div class="buttons">
+      <div class="row">
+        <div class="instagram-caroussel">
+          <div class="arrows">
             <div class="arrow-left">
-              <button><i class="fa-solid fa-chevron-left"></i></button>
-            </div>
-            <div class="insta-button">
-              <button><i class="fa-brands fa-instagram"></i></button>
+              <button @click="previous()"><i class="fa-solid fa-chevron-left"></i></button>
             </div>
             <div class="arrow-right">
-              <button><i class="fa-solid fa-chevron-right"></i></button>
+              <button @click="next()"><i class="fa-solid fa-chevron-right"></i></button>
+            </div>
+          </div>
+          <div class="insta-image" v-for="(image, index) in displayedImages" :key="index">
+            <img :src="image" alt="" />
+            <div class="buttons">
+              <div class="insta-button">
+                <button><i class="fa-brands fa-instagram"></i></button>
+              </div>
             </div>
           </div>
         </div>
@@ -31,14 +35,26 @@
 import { ref } from "vue";
 
 const instagramImages = ref<string[]>([
-  "../assets/Instagram/instaimg(1).jpeg",
-  "src/assets/Instagram/instaimg(2).jpeg",
-  "src/assets/Instagram/instaimg(3).jpeg",
-  "src/assets/Instagram/instaimg(4).jpeg",
-  "src/assets/Instagram/instaimg(5).jpeg",
+ "src/assets/Instagram/instaimg(1).jpeg",
+ "src/assets/Instagram/instaimg(2).jpeg",
+ "src/assets/Instagram/instaimg(3).jpeg",
+ "src/assets/Instagram/instaimg(4).jpeg",
+ "src/assets/Instagram/instaimg(5).jpeg",
 ]);
 
-for (const i of instagramImages.value) {
-  console.log("aqui esta a imagem", i);
+const displayedImages = ref<string[]>(instagramImages.value.slice(0, 3));
+
+function previous() {
+ if (displayedImages.value.length > 1) {
+ const firstImage = displayedImages.value.shift() as string;
+ displayedImages.value.push(firstImage);
+ }
+}
+
+function next() {
+ if (displayedImages.value.length > 1) {
+ const lastImage = displayedImages.value.pop() as string;
+ displayedImages.value.unshift(lastImage);
+ }
 }
 </script>
