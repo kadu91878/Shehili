d6b7353df38e7502c0a2ac004a95593bc2ff2e5b
carlos.eduardo <carlos.eduardo@tce.ma.gov.br>
2023-12-12 15:18:21 -0300

    fix: filter

--- src/components/StorePage.vue
+++ src/components/StorePage.vue
@@ -2,39 +2,50 @@
   <div class="store-page">
     <div class="filter-container">
       <div class="filter-button">
-        <button @click="toggleShow()"><i class="fa-solid fa-filter"></i>Filtro</button>
+        <button @click="toggleShow()">
+          <i class="fa-solid fa-filter"></i>Filtro
+        </button>
       </div>
       <div class="filter-box" :class="{ show: showState }">
         <div class="filter-options">
-          <div class="category-content" v-for="product in catregoryList" :key="product.id">
-            <div class="category-car">
-              <input type="checkbox" :id="product.name" :name="product.name"
-              v-model="catregoryList" :value="product.id"
+          <p>Categorias</p>
+          <div
+            class="category-content"
+            v-for="product in catregoryList"
+            :key="product.id"
+          >
+            <div class="categoria">
+              <input
+                type="checkbox"
+                :id="product.name"
+                :name="product.name"
+                v-model="catregoryList"
+                :value="product.id"
               />
-              <label :for="product.name">{{product.name  }}</label>
-              </div>
+              <label :for="product.name">{{ product.name }}</label>
+            </div>
           </div>
         </div>
         <div class="filter-options">
-          <div class="preco">
             <p>Preco</p>
-            <div class="categoria">
-              <input type="checkbox" id="1-20" name="1-20" />
-              <label for="1-20">1-20R$</label>
+            <div class="category-content">
+              <div class="categoria">
+                <input type="checkbox" id="1-20" name="1-20" />
+                <label for="1-20">1-20R$</label>
+              </div>
+              <div class="categoria">
+                <input type="checkbox" id="20-40" name="20-40" />
+                <label for="20-40">20-40R$</label>
+              </div>
+              <div class="categoria">
+                <input type="checkbox" id="40-60" name="40-60" />
+                <label for="40-60">40-60R$</label>
+              </div>
+              <div class="categoria">
+                <input type="checkbox" id="60+" name="60+" />
+                <label for="60+">60+R$</label>
+              </div>
             </div>
-            <div class="categoria">
-              <input type="checkbox" id="20-40" name="20-40" />
-              <label for="20-40">20-40R$</label>
-            </div>
-            <div class="categoria">
-              <input type="checkbox" id="40-60" name="40-60" />
-              <label for="40-60">40-60R$</label>
-            </div>
-            <div class="categoria">
-              <input type="checkbox" id="60+" name="60+" />
-              <label for="60+">60+R$</label>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -87,12 +98,11 @@ import { ref, computed, onMounted } from "vue";
 import axios, { all } from "axios";
 import type { products } from "../types/products";
 
-
-const showState = ref(false)
+const showState = ref(false);
 
 const toggleShow = () => {
-  showState.value = !showState.value
-}
+  showState.value = !showState.value;
+};
 
 const allItemsList = ref<products[]>([]);
 const selectedItemList = ref<products[]>([]);
@@ -100,7 +110,9 @@ const catregoryList = ref<products[]>([]);
 
 const getProducts = async () => {
   try {
-    const response = await axios.get("https://api.escuelajs.co/api/v1/products");
+    const response = await axios.get(
+      "https://api.escuelajs.co/api/v1/products"
+    );
     allItemsList.value = response.data;
   } catch (eror) {
     console.log(console.error());
@@ -110,7 +122,7 @@ const getProducts = async () => {
 onMounted(async () => {
   await getProducts();
   filterCarsByCategoriaId();
-  console.log("a lista aqui brother", catregoryList.value)
+  console.log("a lista aqui brother", catregoryList.value);
 });
 
 const itemsPerPage = 6;
@@ -133,15 +145,16 @@ const setCurrentPage = (page: number) => {
   currentPage.value = page;
 };
 
-
 const filterCarsByCategoriaId = () => {
   if (selectedItemList.value.length === 0) {
-    const uniqueCategoryNames = [...new Set(allItemsList.value.map(product => product.category.name))];
-    catregoryList.value = uniqueCategoryNames.map(name => ({ name }));
+    const uniqueCategoryNames = [
+      ...new Set(allItemsList.value.map((product) => product.category.name)),
+    ];
+    catregoryList.value = uniqueCategoryNames.map((name) => ({ name }));
   } else {
     catregoryList.value = allItemsList.value
-      .filter(product => selectedItemList.value.includes(product.category.id))
-      .map(product => ({ name: product.category.name }));
+      .filter((product) => selectedItemList.value.includes(product.category.id))
+      .map((product) => ({ name: product.category.name }));
   }
 };
 </script>
