f221ef813213ce1401959e4d21323b2a6434a793
kadu91878 <kadu_91878@hotmail.com>
2023-12-12 15:59:27 -0300

    feat: add product view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ShopView from '../views/ShopView.vue'
+import ProductView from '@/views/ProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/shop',
       name: 'shop',
       component: ShopView
+    },
+    {
+      path:'/product',
+      name: 'product',
+      component: ProductView
     },
   ]
 })
---
+++ src/views/ProductView.vue
@@ -0,0 +1,14 @@
+<template>
+<NavigationBar />
+<ProductPage />
+<Footer />
+
+</template>
+
+
+<script setup lang="ts">
+import NavigationBar from '@/components/NavigationBar.vue';
+import ProductPage from '@/components/ProductPage.vue';
+import Footer from '@/components/Footer.vue';
+
+</script>
